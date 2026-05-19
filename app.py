2346884d513bac85141bca5f004a070232d82d7b
melano <melano@melanos-MacBook-Air.local>
2026-05-19 17:42:45 +0400

    Added a calculation feature

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def login():
 def logout():
     return '<strong>Log out</strong> <button>Click</button>'
 
-
 @app.route('/register')
 def register():
     return '<strong>Register</strong> <button>Click</button>'
@@ -23,5 +22,21 @@ def register():
 def profile():
     return '<strong>profile</strong> <button>Click</button>'
 
+
+@app.route('/calculator/<int:num1>/<int:num2>/<int:num3>/<string:sign>')
+def calculator(num1, num2, num3, sign):
+    try:
+        if sign == '+':
+            return f'<strong>{num1+num2+num3}</strong>'
+        elif sign == '-':
+            return f'<strong>{num1-num2-num3}</strong>'
+        elif sign == ':':
+            return f'<strong>{num1/num2/num3}</strong>'
+        elif sign == 'x':
+            return f'<strong>{num1*num2*num3}</strong>'
+        else:
+            return '<strong>Enter a mathematical operation sign!</strong>'
+    except ZeroDivisionError:
+        return '<strong>You cannot divide by zero</strong>'
 if '__main__' == __name__:
     app.run()
