3a3eea819d6214560f9c86e4b0c6e5272f67ec1b
melano <melano@melanos-MacBook-Air.local>
2026-06-02 20:30:01 +0400

    Added project details page and improved homepage carousel

--- app.py
+++ app.py
@@ -22,12 +22,15 @@ def new_project():
     if request.method == 'POST':
         title = request.form['title']
         description = request.form['description'][:150]
+        full_description = request.form['full_description']
         link = request.form['link']
         project_status = request.form.get('project_status', 'open')
 
         new_created_project = {
+            'id': len(projects_list),
             'title': title,
             'description': description,
+            'full_description': full_description,
             'link': link,
             'project_status': project_status
         }
@@ -46,9 +49,20 @@ def new_project():
     return render_template('new_project.html')
 
 
-@app.route('/current_projects')
+@app.route('/current-projects')
 def current_projects():
     return render_template('current_projects.html', projects=projects_list)
+
+
+@app.route('/project/<int:project_id>')
+def project_details(project_id):
+
+    project = projects_list[project_id]
+
+    return render_template(
+        'project_details.html',
+        project=project
+    )
 
 if __name__ == '__main__':
     app.run()
